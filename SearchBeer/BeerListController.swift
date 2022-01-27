29654a20c5e3ee3314ada7e70b42328587602eae
Mindohyeon <min50875@gmail.com>
2022-01-27 21:20:41 +0900

    [Add] navigation Large Title

--- SearchBeer/BeerListController.swift
+++ SearchBeer/BeerListController.swift
@@ -7,13 +7,41 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 
-class BeerListController : UIViewController {
+class BeerListController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView = UITableView()
+    let items : [String] = ["aa", "bb", "cc"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBlue
         
+        [tableView].forEach{ view.addSubview($0)}
+        
         self.title = "Beer List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.title = "Beer List"
+        
+//        tableView.snp.makeConstraints {
+//            $0.top.equalTo(.snp.bottom)
+//
+//        }
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    
 }
