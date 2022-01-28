ca70b90a6ecee355d498272653dee5f1e11fdae7
Mindohyeon <min50875@gmail.com>
2022-01-28 23:33:32 +0900

    [Add] TabBarController - RandomController

--- SearchBeer/TabBar/TabBarController.swift
+++ SearchBeer/TabBar/TabBarController.swift
@@ -14,23 +14,25 @@ class TabBarController : UITabBarController {
         super.viewDidLoad()
         
         let SearchVC = SearchViewController()
-        
         let BeerListVC = BeerListController()
+        let RandomVC = RandomController()
         
         var SearchViewNC = UINavigationController(rootViewController: SearchVC)
         var BeerListNC = UINavigationController(rootViewController: BeerListVC)
+        var RandomNC = UINavigationController(rootViewController: RandomVC)
         
         SearchViewNC.tabBarItem.title = "Search ID"
         BeerListNC.tabBarItem.title = "Beer List"
+        RandomNC.tabBarItem.title = "Random"
         
         
-        self.setViewControllers([ BeerListNC, SearchViewNC], animated: false)
+        self.setViewControllers([ BeerListNC, SearchViewNC, RandomNC], animated: false)
         
         guard let items = self.tabBar.items else {return}
         
-        let images = ["1.circle", "2.circle"]
+        let images = ["1.circle", "2.circle", "3.circle"]
         
-        for x in 0...1 {
+        for x in 0...2 {
             items[x].image = UIImage(systemName: images[x])
         }
     
