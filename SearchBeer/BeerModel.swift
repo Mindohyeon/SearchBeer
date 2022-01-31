d98c7926592666e2c6f55967ffec2de63d18c80d
Mindohyeon <min50875@gmail.com>
2022-01-31 22:48:44 +0900

    [Change] Json

--- SearchBeer/BeerModel.swift
+++ SearchBeer/BeerModel.swift
@@ -10,12 +10,15 @@ import Foundation
 struct BeerModel : Codable {
     
     // Apple 은 카멜케이스 사용을 권장하기 때문에 swift 에서 카멜케이스를 사용하는 것은 강제된다.
-    var beerName : String
-    var ids : String
+//    var beerName : String
+//    var ids : String
+//
+//    enum CodingKeys : String, CodingKey {
+//        case ids
+//        case beerName = "beer_name"
+//    }
     
-    enum CodingKeys : String, CodingKey {
-        case ids
-        case beerName = "beer_name"
-    }
+    var id : Int
+    var name : String
 }
 
