5a8e5dc8bb733f325607d1a943eea54d574737f8
Mindohyeon <min50875@gmail.com>
2022-02-08 00:12:43 +0900

    tableView 의 Section 개수와 cell 정하기

--- SearchBeer/SearchViewController.swift
+++ SearchBeer/SearchViewController.swift
@@ -7,17 +7,19 @@
 
 import UIKit
 import SnapKit
+import CoreMedia
 
 class SearchViewController: UIViewController {
     
+    var tableView : UITableView!
     
-    var SearchData : [String]?
+    var SearchData = [Beer]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .white
         setupSearchController()
+        setupTableView()
         
         //UISearchBar 내의 텍스트가 변경되는 것을 알린다.
 //        searchController.searchResultsUpdater = self
@@ -25,13 +27,15 @@ class SearchViewController: UIViewController {
     }
     
     func setupSearchController() {
+        self.view.backgroundColor = .white
         let searchController = UISearchController(searchResultsController: nil)
         
         searchController.searchBar.placeholder = "Search"
         navigationItem.searchController = searchController
         
         searchController.searchResultsUpdater = self
-        
+    
+        searchController.hidesNavigationBarDuringPresentation = false
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Search BY ID"
         
@@ -45,3 +49,17 @@ extension SearchViewController : UISearchResultsUpdating {
         dump(searchController.searchBar.text)
     }
 }
+extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SearchData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = SearchData[indexPath.row].name
+        return cell
+    }
+    
+    
+}
+
