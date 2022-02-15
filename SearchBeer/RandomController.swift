4381b40ef921d333c2db3de08b943470db10d2b0
Mindohyeon <min50875@gmail.com>
2022-02-15 23:51:19 +0900

    [Add] RandomBeer data 받아오기

--- SearchBeer/RandomController.swift
+++ SearchBeer/RandomController.swift
@@ -8,7 +8,13 @@
 import Foundation
 import UIKit
 
-class RandomController : UIViewController {
+class RandomController : UITableViewController {
+    var dataTasks = [URLSessionTask]()
+    var beerDetailView = BeerDetailViewController()
+    
+    var beerList : [Beer]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,5 +22,56 @@ class RandomController : UIViewController {
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.title = "Random Beer"
+    
+         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        random()
+        beerDetailView.beer = beerList?[0]
+        print("beerList : \(beerList)")
+
+        
+        self.show(beerDetailView, sender: nil)
+                
+    }
+}
+
+extension RandomController {
+    func random() {
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers/random"),
+                dataTasks.first(where: { $0.originalRequest?.url == url }) == nil
+        else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard error == nil,
+                  let data = data,
+                  let response = response as? HTTPURLResponse,
+                  let beers = try? JSONDecoder().decode([Beer].self, from : data) else {
+                      return 
+                  }
+            
+            switch response.statusCode {
+            case(200...299):
+                print("beers : \(beers)")
+                self?.beerList = beers
+            
+            
+            default:
+                print("""
+                        Error : Error : \(response.statusCode)
+                        Response : \(response)
+                    """)
+                  
+            }
+        }
+        
+        dataTask.resume()
+        
     }
 }
