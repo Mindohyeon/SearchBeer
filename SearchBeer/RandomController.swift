f6bd28f887e4e825bd6419d423eb82df1bc332b6
Mindohyeon <min50875@gmail.com>
2022-02-16 22:22:07 +0900

    randomButton 클릭 시 viewWillAppear 메서드 호출

--- SearchBeer/RandomController.swift
+++ SearchBeer/RandomController.swift
@@ -87,6 +87,7 @@ class RandomController : UIViewController {
     func onTabButton() {
         print("Tabbed")
         random()
+        viewWillAppear(true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
